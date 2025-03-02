696d4876d0f19756040ff453f4cbcc3a264f8c76
XCM42-Orion <64877752+XCM42-Orion@users.noreply.github.com>
2025-03-02 14:55:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,13 @@ from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
 
-@register("helloworld", "Your Name", "一个简单的 Hello World 插件", "1.0.0", "repo url")
+@register("CHaTNE", "Your Name", "CHTNE自用信息插件", "1.0.0", "repo url")
 class MyPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
     
     # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        '''这是一个 hello world 指令''' # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+    @filter.command("about")
+    async def about(self, event: AstrMessageEvent):
+        '''关于茶町CHaTNE''' # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
+        yield event.plain_result(f"茶町CHaTNE是CHTNE化学社基于Koishi和Astrbot框架自研的，支持LLM（大语言模型）的QQbot。未来CHaTNE会添加更多的实用功能。") # 发送一条纯文本消息
